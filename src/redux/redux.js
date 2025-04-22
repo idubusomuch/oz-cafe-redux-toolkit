6079e0c51adca739d2e70f65994d45cd79d461e5
idubusomuch <idubusomuch@gmail.com>
2025-04-22 18:38:53 +0900

    [Redux] React 15일차 과제 제출

---
+++ src/redux/redux.js
@@ -0,0 +1,38 @@
+import data from '../assets/data';
+import { combineReducers, legacy_createStore } from 'redux';
+
+// action
+export const addToCart = (options, quantity, id) => {
+  return {
+    type: 'addToCart',
+    payload: { options, quantity, id },
+  };
+};
+
+export const removeFromCart = (id) => {
+  return {
+    type: 'removeFromCart',
+    payload: { id },
+  };
+};
+
+// Reducer
+const cartReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'addToCart':
+      return [...state, action.payload];
+    case 'removeFromCart':
+      return state.filter((el) => action.payload.id !== el.id);
+    default:
+      return state;
+  }
+};
+
+const menuReducer = (state = data.menu, action) => {
+  return state;
+};
+
+const rootReducer = combineReducers({ cartReducer, menuReducer });
+
+// store
+export const store = legacy_createStore(rootReducer);
